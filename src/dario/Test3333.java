967c33068d41a8b175a4799fcde590c4c9885c41
Dario <dariobruckner@gmail.com>
2017-03-16 10:51:56 +0100

    VERSIOn_00.02

--- src/dario/Test3333.java
+++ src/dario/Test3333.java
@@ -3,5 +3,8 @@ package dario;
 public class Test3333 {
 	public static void main(String [] args){
 		System.out.println("Guten Morgen Hr. BrucknerD");
+		System.out.println("Guten Morgen Hr. BrucknerD");
+		System.out.println("Guten Morgen Hr. BrucknerD");
+		
 	}
 }
